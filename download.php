4f67ca5935dfee96aeaccb21b224e7f52e25c1be
mrfender <mfender@gmail.com>
2015-07-09 07:23:21 +0200

    php test

--- download.php
+++ download.php
@@ -1,4 +1,12 @@
 <?php
 
+    echo "<br><Br>POST:";
+    print_r("$_POST");
+    
+    echo "<br><Br>GET:";
+    print_r("$_GET");
+    
+    echo "<br><Br>SESSION:";
+    print_r("$_SESSION");
 
 ?>
