17b5300ca1bd331b57012d269eb14717f11042b6
mrfender <mfender@gmail.com>
2015-07-21 22:44:27 +0200

    test

--- download.php
+++ download.php
@@ -1,42 +1,17 @@
 <?php
 
-    /*echo "<br><Br>POST:";
-    print_r("$_POST");
+    include("paypal.php");
     
-    echo "<br><Br>GET:";
-    print_r("$_GET");
-    
-    echo "<br><Br>SESSION:";
-    print_r("$_SESSION");
-    
-    
-    // connect
-    $m = new MongoClient();
-    
-    // select a database
-    $db = $m->store;
-    
-    // select a collection (analogous to a relational database's table)
-    $collection = $db->files;
-    
-    // add a record
-    $document = array( "filename" => "Yldau - Fjoer.mp3", "location" => "downloads\\" );
-    $collection->insert($document);
-
-    // add another record, with a different "shape"
-    $document = array( "filename" => "JoY-project - Hold on.mp3", "location" => "downloads\\" );
-    $collection->insert($document);
-
-    // find everything in the collection
-    $cursor = $collection->find();
-
-    // iterate through the results
-    foreach ($cursor as $document) {
-        echo $document["filename"] . "\n";
-    }*/
-    
-    echo "download page";
-    
-    print_r($_POST);
+    if ( $txn_id = $_POST['txn_id'] ) {
+        $fetch = $mysqli->query("SELECT * FROM tokens WHERE txnID='$txn_id' AND elapseDT < DATE_ADD(NOW(), INTERVAL 14 DAY)");
+        
+        if ( $fetch->num_rows > 1 ) {
+            echo "Products bought:<br><br>";
+            while($product = mysqli_fetch_assoc($fetch)) {
+                print_r($product);
+                echo "<br><br>";
+            }
+        }
+    }
 
 ?>
